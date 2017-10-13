9b46e737db01c42faa2049a640e844ec3de1a038
chrisbrickey <cbrickey1@gmail.com>
2017-10-13 00:25:17 -0700

    push react code from entry file through index.html

---
+++ frontend/entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('react');
+  ReactDOM.render(
+    <div>on entry.jsx</div>, root);
+});

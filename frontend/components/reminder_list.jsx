c49f781856c1b1ab5092bccd31c74606177990a8
chrisbrickey <cbrickey1@gmail.com>
2017-10-13 09:04:04 -0700

    show titles of reminders on home screen

--- frontend/components/reminder_list.jsx
+++ frontend/components/reminder_list.jsx
@@ -4,12 +4,20 @@ class ReminderList extends React.Component {
 
   render() {
     console.log(this.props);
+    const { reminders } = this.props; //the array of reminders
+    console.log(reminders);
+
+    const reminderItems = reminders.map(reminder => (
+      <li>{ reminder.title }</li>
+    ) //end of body; doesn't include parameter
+  ); //end of anonymous function
 
     return(
-      <div>
-        from the reminder_list presentational component
+      <div className="reminderList">
+        <h6>from the reminder_list presentational component</h6>
+        <ul> { reminderItems } </ul>
       </div>
-    );
+    ); //end of contents being returned
 
   } //end of render function
 

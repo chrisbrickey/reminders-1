654076d8537e7db60957ca295a3b95ffe819bda2
chrisbrickey <cbrickey1@gmail.com>
2017-10-13 01:43:45 -0700

    establish redux store and make available in console

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 
 document.addEventListener("DOMContentLoaded", () => {
+  const preloadedState = localStorage.state ? JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const root = document.getElementById('react');
   ReactDOM.render(
     <div>on entry.jsx</div>, root);
